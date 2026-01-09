62dc556acd5d50bcafdfc2ac5e741026167c686a
Gregory <cgregg@belgacom.net>
2026-01-09 04:00:32 +0100

    feat(users): implement UsersService with Prisma integration and validation
    - Add create() method with strict typing and unique email conflict handling.
    - Add findAll() method ordered by creation date.
    - Inject PrismaService and use generated Prisma types.
    - Improve error handling using PrismaClientKnownRequestError.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '@core/database/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Prisma, user } from '../../generated/prisma';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  /**
+   * Create a new user.
+   * Throws ConflictException if the email already exists.
+   */
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<user> {
+    try {
+      return await this.prismaService.user.create({
+        data: createUserDto,
+      });
+    } catch (error: unknown) {
+      // On vérifie si l'erreur vient de Prisma
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // Prisma unique constraint violation
+        if (error.code === 'P2002') {
+          throw new ConflictException('This email is already registered.');
+        }
+      }
+      throw error;
+    }
+  }
+  /**
+   * Return all users ordered by creation date (newest first).
+   */
+  async findAll(): Promise<user[]> {
+    return this.prismaService.user.findMany({
+      orderBy: { createdAt: 'desc' }, // les plus récents en premier
+    });
+  }
+}
