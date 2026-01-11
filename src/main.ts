45f34e81ecf34fb997a27af56fd03e3c730f9bb7
gregory <cgregg@belgacom.net>
2026-01-11 23:13:41 +0100

    fix(docs): restore swagger access and align nestjs v11 versions

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,18 @@ import { setupGlobalSettings } from '@core/config/globals-setup.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configurer les pipes et le préfixe /api/v1
   setupGlobalSettings(app);
 
-  await app.init(); // Garanti que tous les modules sont bien initialisés
-
+  // Swagger
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    // Cette option est CRUCIALE quand on utilise un Global Prefix
+    // Elle permet d'accéder à Swagger sur /api au lieu de /api/v1/api
+    useGlobalPrefix: false,
+  });
 
+  // On lance tout (init() sera appelé en interne ici)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
