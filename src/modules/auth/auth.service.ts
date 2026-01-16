95db9ffe69770ef12d8f9b6420db85eee40ac8e4
Gregory <cgregg@belgacom.net>
2026-01-16 23:05:45 +0100

    feat(auth): implement validation and registration logic in AuthService
    - Integrated UsersService to fetch and create users.
    - Implemented validateUser using argon2 to verify credentials.
    - Added login and register core logic.
    - Used UserAuthEntity for secure internal data flow.
    
    Ref #26

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,16 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { UsersService } from '@modules/users/services/users.service';
+import { UserEntity } from '@modules/users/entities/user.entity';
+import * as argon2 from 'argon2';
+import { UserAuthEntity } from '@modules/auth/entities/user-auth.entity';
 
 @Injectable()
 export class AuthService {
-  login(dto: LoginDto) {
-    // Implémentation à venir
-    return { message: 'login not implemented yet', dto };
+  // Injection du service Users
+  constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * Logique de validation interne
+   * Utilisée pour vérifier les credentials avant de générer un token
+   */
+  async validateUser(email: string, pass: string): Promise<Omit<UserEntity, 'password'> | null> {
+    const user: UserAuthEntity | null = await this.usersService.findByEmail(email);
+
+    // Comparaison du hash Argon2
+    if (user && (await argon2.verify(user.password, pass))) {
+      // on extrait le password pour ne pas le renvoyer par erreur !
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 
-  register(dto: RegisterDto) {
-    // Implémentation à venir
-    return { message: 'register not implemented yet', dto };
+  async login(dto: LoginDto) {
+    const user: any = await this.validateUser(dto.email, dto.password);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password.');
+    }
+
+    // pour l'instant retourne un user en attente du JWT
+    // Todo : JWT
+    return {
+      message: 'Login successful',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      user: user,
+    };
+  }
+
+  /**
+   * Logique de création de compte (Register)
+   * On délègue au UsersService qui gère déjà le hashage et Prisma
+   */
+  async register(dto: RegisterDto) {
+    // Appel du service Users pour la création d'un nouvel utilisateur.
+    return await this.usersService.create(dto);
   }
 }
