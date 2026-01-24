d739b9881c6bdc127a27c3bf67a899ce123817aa
gregg-laptop <cgregg@belgacom.net>
2026-01-24 02:06:28 +0100

    feat(auth): add JwtStrategy for validating JWT tokens

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '@modules/users/users.module';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'CHANGE_THIS_SECRET',
+      secret: process.env.JWT_SECRET || 'votre_secret_de_secours',
       signOptions: { expiresIn: '1h' },
     }),
   ],
